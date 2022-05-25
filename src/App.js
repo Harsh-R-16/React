fbb37347aaad06aea7af8fd1d79b4b2a1fc63f46
Harsh Gajera <harsh.gajera17@gmail.com>
2022-05-25 20:36:51 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Debounce from "./Components/Debounce/Debounce";
-import Fetch from "./Components/Fetch/Fetch";
-import Progress from "./Components/Progress/Progress";
-import Toggle from "./Components/Toggle/Toggle";
-import Recursion from "./Components/Recursion/Recursion";
-import Slider from "./Components/Slider/Slider";
-import Scroll from "./Components/Scroll/Scroll";
-import Timer from "./Components/Timer/Timer";
-import Comments from "./Components/Comments/Main";
+// import Debounce from "./Components/Debounce/Debounce";
+// import Fetch from "./Components/Fetch/Fetch";
+// import Progress from "./Components/Progress/Progress";
+// import Toggle from "./Components/Toggle/Toggle";
+// import Recursion from "./Components/Recursion/Recursion";
+// import Slider from "./Components/Slider/Slider";
+// import Scroll from "./Components/Scroll/Scroll";
+// import Timer from "./Components/Timer/Timer";
+// import Comments from "./Components/Comments/Main";
 import Matrix1 from "./Components/Matrix/Matrix1";
 import Matrix2 from "./Components/Matrix/Matrix2";
 function App() {
